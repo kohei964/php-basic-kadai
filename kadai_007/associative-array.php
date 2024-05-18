9059c92aa3ae9d15aed7d56bd7c9580cde749ec7
kohei964 <163335760+kohei964@users.noreply.github.com>
2024-05-19 00:15:48 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>7章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+            $vegetables = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+            print_r($vegetables);
+        ?>
+    </p>
+</body>
+</html>
